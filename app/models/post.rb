66d67527c1a80edb1620e5c8c93317f1595dcc86
安斎亮 <anzaitoru@anzairyounoMacBook-Air.local>
2022-04-26 16:47:47 +0900

    バリデーションの分岐の修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,29 @@ class Post < ApplicationRecord
   validates :start, presence: true
   validates :finish, presence: true
   validates :memo, length: {in: 1..140}
+
+  validate :date_before_start
+  validate :date_before_end
+
+  private
+
+  require "date"
+
+
+  def date_before_start
+    if start < Date.today
+      errors.add(:start, "は今日以降の日付を選択してください")
+    end
+  end
+
+  def date_before_end
+    if finish < start || finish < Date.today
+      errors.add(:finish, "は開始日以降の日付を選択してください")
+    end
+  end
+
+
+
+
 end
 
